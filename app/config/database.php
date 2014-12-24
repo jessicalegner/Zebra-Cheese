455d8e23bc75769164201940500eb04843b025c1
Jessica Legner <jessicalegner@leggyj.local>
2014-12-23 23:57:13 -0500

    setting up db configs

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'ZebraCheese',
+			'username'  => 'root',
+			'password'  => 'oh27ap',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
